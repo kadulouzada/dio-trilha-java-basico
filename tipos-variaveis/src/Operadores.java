faee65cc7545161392cd416f436d4c984d0d245c
kadulouzada <kadu_louzada@hotmail.com>
2026-05-30 22:57:11 -0300

    20260530

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,6 @@
+public class Operadores {
+	public static void main(String[] args) {
+		String nomeCompleto = "LINGUAGEM" + "JAVA"; // fixed: valid Java class and main method
+		System.out.println(nomeCompleto);
+	}
+}
