a167f66d57511894df1ffa01a412c3cdc0f7373d
kadulouzada <kadu_louzada@hotmail.com>
2026-06-04 21:14:26 -0300

    EntregaProjeto

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,43 @@
+import java.util.Scanner;
+import java.util.Locale;
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        scanner.useLocale(Locale.US);
+
+        System.out.println("Por favor, digite o número da Agência !");
+        String agencia = scanner.nextLine();
+
+        System.out.println("Por favor, digite o número da Conta !");
+        int numero = scanner.nextInt();
+        scanner.nextLine(); // Consumir a quebra de linha
+
+        System.out.println("Por favor, digite o nome do Cliente !");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.println("Por favor, digite o saldo !");
+        double saldo = scanner.nextDouble();
+
+        String mensagem = "Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.";
+        System.out.println(mensagem);
+
+        scanner.close();
     }
 }
+
+/**Dentro do projeto, crie a classe ContaTerminal.java para realizar toda a codificação do nosso programa.
+Revise sobre regras de declaração de variáveis
+Atributo	Tipo	Exemplo
+Numero	Inteiro	1021
+Agencia	Texto	067-8
+Nome Cliente	Texto	MARIO ANDRADE
+Saldo	Decimal	237.48
+Revise sobre terminal, main args e a classe Scanner
+Permita que os dados sejam inseridos via terminal sendo que o usuário receberá a mensagem de qual informação será solicitada, exemplo:
+Programa: "Por favor, digite o número da Agência !"
+Usuário: 1021 (depois ENTER para o próximo campo)
+Revise sobre concatenação e classe String com método concat
+Depois de todas as informações terem sido inseridas, o sistema deverá exibir a seguinte mensagem:
+"Olá [Nome Cliente], obrigado por criar uma conta em nosso banco, sua agência é [Agencia], conta [Numero] e seu saldo [Saldo] já está disponível para saque".
+
+Os campos em [ ] devem ser alterados pelas informações que forem inseridas pelos usuários. */
